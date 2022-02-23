6a2e00a7313970198c84458c661e1886a8fa9832
AnastasiiaDedela <nastyadedela@gmail.com>
2022-02-23 17:14:32 +0200

    feat: completed js file for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  let neededStack = [];
+
+  for (let bracket of str) {
+      let bracketsIndex = brackets.indexOf(bracket);
+      if (bracketsIndex % 2 === 0) {
+          if (bracket === brackets[bracketsIndex + 1] && neededStack[neededStack.length - 1] === bracketsIndex) {
+              neededStack.pop();
+          } else if (bracket === brackets[bracketsIndex + 1] && neededStack[neededStack.length - 1] !== bracketsIndex) {
+              neededStack.push(bracketsIndex);
+          } else {
+              neededStack.push(bracketsIndex);
+          }
+      }  else {
+          if (neededStack.pop() !== bracketsIndex-1) {
+              return false;
+          }
+      }
+      
+  }
+  return neededStack.length === 0;
 }
